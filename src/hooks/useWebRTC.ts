906032620b814437781e597f25616f243d599d13
Vrushank Bhavsar <vrushankbhavsar1706@gmail.com>
2026-04-14 12:32:39 +0530

    fix: add TURN servers for reliable WebRTC connections

--- src/hooks/useWebRTC.ts
+++ src/hooks/useWebRTC.ts
@@ -39,6 +39,21 @@ export const useWebRTC = () => {
             { urls: 'stun:stun2.l.google.com:19302' },
             { urls: 'stun:stun3.l.google.com:19302' },
             { urls: 'stun:stun4.l.google.com:19302' },
+            {
+              urls: 'turn:openrelay.metered.ca:80',
+              username: 'openrelayproject',
+              credential: 'openrelayproject'
+            },
+            {
+              urls: 'turn:openrelay.metered.ca:443',
+              username: 'openrelayproject',
+              credential: 'openrelayproject'
+            },
+            {
+              urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+              username: 'openrelayproject',
+              credential: 'openrelayproject'
+            }
           ]
         }
       };
